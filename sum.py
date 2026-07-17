ff387ee6429d7de52c9ac07a38a12e220baf6861
aashish <aashishkarkix@gmail.com>
2026-07-17 07:24:07 +0545

    feat: added two number

---
+++ sum.py
@@ -0,0 +1,11 @@
+def sum_numbers(numbers):
+    """
+    This function takes a list of numbers and returns their sum.
+
+    :param numbers: List of numbers to be summed
+    :return: Sum of the numbers
+    """
+    total = 0
+    for number in numbers:
+        total += number
+    return total
